026192ff8186e6b1861ae12f2bebc1952ae93171
IPhantom <ivannn787@gmail.com>
2014-07-10 22:54:04 +0300

    added partial application, currying, map

--- JS-Advanced/Scripts/index.js
+++ JS-Advanced/Scripts/index.js
@@ -1,6 +1,52 @@
 (function() {
     var tool = new FeaturesNS.Funcs();
 
+    //Problem 1: Partial Application//
+    function mul() {
+        var result = 1;
+
+        for (var i = 0; i < arguments.length; i++) {
+            result *= arguments[i];
+        }
+
+        return result;
+    }
+
+    var mul2 = tool.partial(mul, 1, 2);
+    mul2(3, 4, 5);
+    mul2(3, 4, 5, 6, 7);
+
+    var mul4 = tool.partial(mul, 1, 2, 3, 4);
+    mul4(5, 6, 7);
+    mul4(5, 6, 7, 8, 9);
+
+    //Problem 2: Currying//
+    var ind = 0;
+    function a(x, y, z) {
+        console.log(++ind + ': ' + x + ' and ' + y + ' or ' + z);
+    };
+
+    a('x', 'y', 'z');       // "1: x and y or z"
+
+    var b = tool.curry(a);
+    b();                    // nothing logged, `a` not invoked
+    b('x');                 // nothing logged, `a` not invoked
+    b('x', 'y');            // nothing logged, `a` not invoked
+    b('x')('y');            // nothing logged, `a` not invoked
+    b('x')('y')('z');       // "2: x and y or z"
+    b('x', 'y', 'z');       // "3: x and y or z"
+
+    var c = tool.curry(a, 'x');
+    c();                    // nothing logged, `a` not invoked
+    c('y');                 // nothing logged, `a` not invoked
+    c('y', 'z');            // "4: x and y or z"
+    c('y')('z');            // "5: x and y or z"
+
+    //Problem 5: Map//
+    var numbers = [1, 4, 9, 16, 25];
+    var roots = tool.map(numbers, Math.sqrt);
+    console.log(roots);
+
     //Problem 9: First//
     var array = [{
             name: 'Alex',
--- JS-Advanced/Scripts/module.js
+++ JS-Advanced/Scripts/module.js
@@ -25,7 +25,55 @@ FeaturesNS.createNS = function (namespace) {
 FeaturesNS.createNS("FeaturesNS.Funcs");
 
 FeaturesNS.Funcs = function () {
-    
+
+    function partial(fn /*, args...*/) {
+        var slice = Array.prototype.slice,
+            args = slice.call(arguments, 1);
+
+        return function () {
+            var t = args.concat(slice.call(arguments));
+            return fn.apply(this, t);
+        };
+    }
+
+    function curry(/* n,*/ fn /*, args...*/) {
+        var n,
+          slice = Array.prototype.slice,
+          origArgs = slice.call(arguments, 1);
+
+        if (typeof fn === 'number') {
+            n = fn;
+            fn = origArgs.shift();
+        } else {
+            n = fn.length;
+        }
+
+        return function () {
+            var args = origArgs.concat(slice.call(arguments));
+
+            return args.length < n
+              ? curry.apply(this, [n, fn].concat(args))
+              : fn.apply(this, args);
+        };
+    };
+
+    function map(arr, fn /*, thisp*/) {
+        
+        if (typeof fn != "function")
+            throw new TypeError();
+
+        var len = arr.length;
+        var res = new Array(len);
+        var thisp = arguments[2];
+
+        for (var i = 0; i < len; i++) {
+            if (i in arr)
+                res[i] = fn.call(thisp, arr[i], i, arr);
+        }
+
+        return res;
+    };
+
     function first(arr, predicateCallback) {
         if (typeof predicateCallback !== 'function') {
             return undefined;
@@ -36,9 +84,9 @@ FeaturesNS.Funcs = function () {
         }
 
         return undefined;
-    }
+    };
 
-    function memoize(func) {
+    function memoize(fn) {
         var cache = {};
         var slice = Array.prototype.slice;
 
@@ -48,11 +96,14 @@ FeaturesNS.Funcs = function () {
             if (args in cache)
                 return cache[args];
             else
-                return (cache[args] = func.apply(this, args));
+                return (cache[args] = fn.apply(this, args));
         };
     };
 
     var api = {
+        partial: partial,
+        curry: curry,
+        map: map,
         first: first,
         memoize: memoize,
     };
