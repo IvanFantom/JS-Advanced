34f50a568b757c282d7b6bb73a95daf8cfc67b10
IPhantom <ivannn787@gmail.com>
2014-07-13 23:29:04 +0300

    solved 7th, 8th problems; adding demo for memoization

--- JS-Advanced/Scripts/index.js
+++ JS-Advanced/Scripts/index.js
@@ -2,6 +2,8 @@
     var tool = new FeaturesNS.Funcs();
 
     //Problem 1: Partial Application//
+    console.log('Problem 1: Partial Application');
+
     function mul() {
         var result = 1;
 
@@ -13,14 +15,16 @@
     }
 
     var mul2 = tool.partial(mul, 1, 2);
-    mul2(3, 4, 5);
-    mul2(3, 4, 5, 6, 7);
+    console.log(mul2(3, 4, 5));
+    console.log(mul2(3, 4, 5, 6, 7));
 
     var mul4 = tool.partial(mul, 1, 2, 3, 4);
-    mul4(5, 6, 7);
-    mul4(5, 6, 7, 8, 9);
+    console.log(mul4(5, 6, 7));
+    console.log(mul4(5, 6, 7, 8, 9));
 
     //Problem 2: Currying//
+    console.log('\nProblem 2: Currying');
+
     function add(a, b, c) {
         var total = a + b + c;
         return a + '+' + b + '+' + c + '=' + total;
@@ -35,8 +39,10 @@
     console.log(add1and2curry(4)); // "1+2+4=7"
 
     //Problem 3: Linear fold//
-    var someArray = [1, 10, 100];
-    var addCallback = function (prev, curr, ind, arr) {
+    console.log('\nProblem 3: Linear fold');
+
+    var someArray = [1, 10, 100, -5, 27, 42, 0, -17, 0.85, 1.24];
+    var addCallback = function (prev, curr, ind, list) {
         return prev + curr;
     };
 
@@ -44,22 +50,35 @@
     console.log(tool.fold(someArray, addCallback, 1000));
 
     //Problem 5: Map//
+    console.log('\nProblem 5: Map');
+
     var numbers = [1, 4, 9, 16, 25];
     var roots = tool.map(numbers, Math.sqrt);
     console.log(roots);
 
     //Problem 6: Filter//
+    console.log('\nProblem 6: Filter');
+
     var arr = [1, 2, 3, 4, 5];
     var odd = tool.filter(arr, function (val) {
         return 0 != val % 2;
     });
     console.log(odd);
-    var even = tool.filter(arr, function (val) {
+    
+    //Problem 7: Average of even numbers//
+    console.log('\nProblem 7: Average of even numbers');
+
+    var nums = [1, 23, 2, 6, 12, 0];
+    var even = tool.filter(nums, function(val) {
         return 0 == val % 2;
     });
     console.log(even);
+    var average = tool.fold(even, addCallback) / even.length;
+    console.log(average);
 
     //Problem 9: First//
+    console.log('\nProblem 9: First');
+
     var array = [{
             name: 'Alex',
             age: 22
